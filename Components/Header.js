f19ce55134379768e0718991b72e76035f643e12
davidkamere <davidkamere@live.com>
2022-06-13 15:24:41 +0300

    Add header(initial commit)

---
+++ Components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="py-5 px-12 shadow-sm border-b bg-white sticky top-o z-50">
+            <div className="flex items-center justify-end space-x-4">
+                <button>Logout</button>
+            </div>
+        </div>
+    )
+}
+export default Header
