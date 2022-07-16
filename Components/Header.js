40b6f8b589a56c0545157fe766bf499fa2a6088a
davidkamere <davidkamere@live.com>
2022-07-17 02:27:02 +0300

    fix breaking serverprops changes

--- Components/Header.js
+++ Components/Header.js
@@ -3,8 +3,8 @@ import { useRouter } from "next/router"
 import { UserCircleIcon } from "@heroicons/react/solid"
 import Link from 'next/link'
 
-const Header = () => {
-    const { data: session} = useSession()
+const Header = (props) => {
+    const { data: session } = useSession()
     const router = useRouter()
 
 
@@ -13,13 +13,14 @@ const Header = () => {
         router.push('/auth/signin')
     }
 
+        console.log(session)
     return (
    
         <div className="pt-5 px-8">
             <div className="flex justify-between items-center">
             <Link href="/"><p className="tracking-wide font-bold hover:cursor-pointer">PCHA</p></Link>
                 <div className="flex flex-row gap-5">
-                    <Link href={`/profile/${session.user.username}`}>
+                    <Link href={`/profile/${session?.user?.username}`}>
                         <UserCircleIcon className="h-9 w-9 my-1 "/>
                     </Link>
                     <button onClick={signInPage} className="py-2 px-4 bg-gradient-to-r from-[#adda89] to-[#fdf9a1] rounded-lg text-white">Sign out</button>
@@ -32,3 +33,9 @@ const Header = () => {
     )
 }
 export default Header
+
+export async function getServerSideProps(context) {
+    return {
+      props: {}, // will be passed to the page component as props
+    };
+  }
