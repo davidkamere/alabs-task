a696a04a73f78c6586e32fee6a2c8845ff54b823
davidkamere <davidkamere@live.com>
2022-07-23 15:47:08 +0300

    Mininal UI changes

--- Components/Loading.js
+++ Components/Loading.js
@@ -3,9 +3,9 @@ import Image from "next/future/image"
 const Loading = () => {
     return (
         <div className="flex justify-center items-center h-screen bg-white">
-            <div className="animate-pulse flex justify-center text-3xl font-bold mb-8 text-[#fdf9a1] tracking-widest">
+            <div className="animate-pulse flex justify-center text-3xl font-bold mb-8 text-black tracking-widest">
                 <Image src="/floral.ico" width={30} height={30} className="mr-4"/>
-                <span className="text-black">P</span>CHA
+                <span className="text-[#9ea659]">P</span>CHA
             </div>
         </div>
       )
